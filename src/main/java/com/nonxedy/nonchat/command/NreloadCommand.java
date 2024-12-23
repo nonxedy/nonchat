19a2078279fd5508a0751d0c8dee00c4532404a8
nonxedy <pos18411@gmail.com>
2024-12-23 09:42:24 +0300

    Update 6.7.2-pt

--- src/main/java/com/nonxedy/nonchat/command/NreloadCommand.java
+++ src/main/java/com/nonxedy/nonchat/command/NreloadCommand.java
@@ -1,18 +1,22 @@
 package com.nonxedy.nonchat.command;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 
 import com.nonxedy.nonchat.nonchat;
 import com.nonxedy.nonchat.config.PluginMessages;
 import com.nonxedy.nonchat.utils.ColorUtil;
 
-public class NreloadCommand implements CommandExecutor {
+public class NreloadCommand implements CommandExecutor, TabCompleter {
 
-    private nonchat plugin;
-    private PluginMessages messages;
+    private final nonchat plugin;
+    private final PluginMessages messages;
 
     public NreloadCommand(nonchat plugin, PluginMessages messages) {
         this.plugin = plugin;
@@ -20,31 +24,59 @@ public class NreloadCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
-        plugin.logCommand(command.getName(), args);
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, 
+                        @NotNull String label, @NotNull String[] args) {
         
-        if (!sender.hasPermission("nonchat.nreload")) {
-            sender.sendMessage(ColorUtil.parseComponent(messages.getString("no-permission")));
-            plugin.logError("You don't have permission to reload the config.");
+        if (!hasReloadPermission(sender)) {
             return true;
         }
 
-        sender.sendMessage(ColorUtil.parseComponent(messages.getString("reloading")));
-        plugin.logResponse("Reloading...");
+        performReload(sender);
+        return true;
+    }
 
-        try {
-            plugin.reloadConfig();
-            plugin.reloadDebugger();
-            plugin.stopAutoBroadcastSender();
-            plugin.registerUtils();
-            sender.sendMessage(ColorUtil.parseComponent(messages.getString("reloaded")));
-
-            plugin.logResponse("Config reloaded.");
-        } catch (Exception e) {
-            sender.sendMessage(ColorUtil.parseComponent(messages.getString("reload-failed")));
-
-            plugin.logError("There was an error reloading the config: " + e.getMessage());
+    private boolean hasReloadPermission(CommandSender sender) {
+        if (!sender.hasPermission("nonchat.nreload")) {
+            sendNoPermissionMessage(sender);
+            return false;
         }
         return true;
+    }
+
+    private void sendNoPermissionMessage(CommandSender sender) {
+        sender.sendMessage(ColorUtil.parseComponent(messages.getString("no-permission")));
+        plugin.logError("Permission denied: nonchat.nreload");
+    }
+
+    private void performReload(CommandSender sender) {
+        sender.sendMessage(ColorUtil.parseComponent(messages.getString("reloading")));
+        plugin.logResponse("Initiating config reload...");
+
+        try {
+            executeReload();
+            sender.sendMessage(ColorUtil.parseComponent(messages.getString("reloaded")));
+            plugin.logResponse("Configuration reload successful");
+        } catch (Exception e) {
+            handleReloadError(sender, e);
+        }
+    }
+
+    private void executeReload() {
+        plugin.reloadConfig();
+        plugin.reloadDebugger();
+        plugin.stopAutoBroadcastSender();
+        plugin.registerUtils();
+    }
+
+    private void handleReloadError(CommandSender sender, Exception e) {
+        sender.sendMessage(ColorUtil.parseComponent(messages.getString("reload-failed")));
+        plugin.logError("Configuration reload failed: " + e.getMessage());
+        e.printStackTrace();
+    }
+
+    @Override
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command,
+                                        @NotNull String label, @NotNull String[] args) {
+        return Collections.emptyList();
     }
 }
