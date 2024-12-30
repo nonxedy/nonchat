04bd870819b17b833a8e16d59553356beb262f3a
nonxedy <pos18411@gmail.com>
2024-12-31 00:04:03 +0300

    Update 6.8.0-pt

--- src/main/java/com/nonxedy/nonchat/listeners/DeathListener.java
+++ src/main/java/com/nonxedy/nonchat/listeners/DeathListener.java
@@ -2,38 +2,41 @@ package com.nonxedy.nonchat.listeners;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import com.nonxedy.nonchat.config.PluginConfig;
+import com.nonxedy.nonchat.utils.ColorUtil;
 
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 
-@SuppressWarnings("deprecation")
 public class DeathListener implements Listener {
 
-    private PluginConfig config;
+    private final PluginConfig config;
 
     public DeathListener(PluginConfig config) {
         this.config = config;
     }
     
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
         User user = LuckPermsProvider.get().getUserManager().getUser(player.getUniqueId());
+        
+        if (user == null || config.getDeathFormat() == null) {
+            return;
+        }
+
         String prefix = user.getCachedData().getMetaData().getPrefix();
         String suffix = user.getCachedData().getMetaData().getSuffix();
 
+        String deathMessage = config.getDeathFormat()
+            .replace("{prefix}", prefix != null ? prefix : "")
+            .replace("{suffix}", suffix != null ? suffix : "")
+            .replace("{player}", player.getName());
 
-        String deathFormat = config.getDeathFormat();
-        if (deathFormat != null) {
-            deathFormat = deathFormat.replace("{prefix}", prefix != null ? prefix : "");
-            deathFormat = deathFormat.replace("{suffix}", suffix != null ? suffix : "");
-            deathFormat = deathFormat.replace("{player}", player.getName());
-
-            event.setDeathMessage(deathFormat);
-        }
+        event.deathMessage(ColorUtil.parseComponent(deathMessage));
     }
 }
